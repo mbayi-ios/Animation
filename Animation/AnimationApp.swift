1909caa8ab0a24669d3d8558e4136082d150c89a
Ambrose Mbayi <91916741+mbayi-ios@users.noreply.github.com>
2023-09-01 10:17:06 +0300

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Ambrose Mbayi on 01/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
